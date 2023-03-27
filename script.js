02aff0f78d29f9a027f9bf3612472d10a9cc20e7
Oskar-fluga-bzzz <oskar.hughes@skola.taby.se>
2023-03-27 09:20:37 +0200

    my wife's son

--- script.js
+++ script.js
@@ -9,8 +9,17 @@ canvas.style.height = "100%";
 // Genom variabeln c kommer man åt det mesta som
 // med canvas att göra
 const c = canvas.getContext("2d");
+c.fillStyle = "#FF0000";
 const midX = canvas.width / 2;
 const midY = canvas.height / 2;
+// Create gradient
+var grd = c.createLinearGradient(0, 0, 800, 0);
+grd.addColorStop(0, "blue");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+c.fillStyle = grd;
+c.fillRect(500, 250, 400, 300);
 
 // Matar ut canvas dimensioner till konsolen, pröva att
 // ändra storlekn på fönstret och ladda däörefter om sidan.
@@ -21,5 +30,10 @@ Höjd på canvas: ${canvas.height}`
 
 function drawPicture() {
   // Här skriver du funktionen som ritar bilden
+  c.beginPath();
+c.moveTo(500, 250);
+c.lineTo(650, 100);
+c.lineTo(900, 250);
+c.stroke();
 }
 drawPicture();
